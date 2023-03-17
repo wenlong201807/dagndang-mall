f95db16a6d73753890584941510fd6e65341c14c
dragon <1573511441@qq.com>
2023-03-17 21:09:44 +0800

    feat: 多环境配置代理

--- dangdang-fe/vite.config.ts
+++ dangdang-fe/vite.config.ts
@@ -1,8 +1,60 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, CommonServerOptions } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import dotenv, { DotenvParseOutput } from 'dotenv';
+import path, { join } from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/dang', //url中的基础路径，当多个项目部署在同一个服务器时，可能用得上
-  plugins: [vue()],
-})
+// 对象形式
+// export default defineConfig({
+//   plugins: [vue()]
+// })
+// 函数形式
+export default defineConfig((mode) => {
+  const envFileName: string = '.env';
+  const curEnvFileName = `${envFileName}.${mode.mode}`;
+  console.log('curEnvFileName', curEnvFileName);
+  let server: CommonServerOptions = {};
+  const envData = fs.readFileSync(curEnvFileName);
+  const envMap: DotenvParseOutput = dotenv.parse(envData); // 解析 key值对形式
+  console.log('envMap', envMap);
+  if (mode.mode === 'development') {
+    console.log('development');
+    server = {
+      host: envMap.VITE_HOST,
+      // host: '172.20.10.4',
+      port: parseInt(envMap.VITE_PORT),
+      proxy: {
+        [envMap.VITE_BASE_URL]: {
+          target: envMap.VITE_PROXY_DOMAIN,
+        },
+      },
+    };
+  } else if (mode.mode === 'production') {
+    console.log('production');
+    server = {
+      host: envMap.VITE_HOST,
+      port: parseInt(envMap.VITE_PORT),
+    };
+  }
+
+  console.log(mode.mode, server);
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // 指定symbolId格式
+        symbolId: 'icon-[name]',
+      }),
+    ],
+    server,
+    resolve: {
+      alias: {
+        '@': join(__dirname, '/src'),
+      },
+    },
+  };
+});
