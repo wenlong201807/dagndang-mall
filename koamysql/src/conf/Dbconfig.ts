302383caa1044e51cf5865abfe2da95280cb9cd6
dragon <1573511441@qq.com>
2023-03-19 00:49:31 +0800

    feat: mysql扩展环境变量

--- koamysql/src/conf/Dbconfig.ts
+++ koamysql/src/conf/Dbconfig.ts
@@ -1,6 +1,7 @@
-function isString(data: any): data is string {
-  return typeof data === 'string'
-}
+// ts 类型守卫
+// function isString(data: any): data is string {
+//   return typeof data === 'string'
+// }
 
 interface DbConConf {
   host: string
@@ -13,6 +14,7 @@ interface DbConConf {
 // 环境
 interface EnvConf {
   dev: DbConConf
+  test: DbConConf
   prod: DbConConf
 }
 
@@ -21,14 +23,20 @@ class Conf {
   env!: keyof EnvConf
   envConf!: EnvConf
   constructor() {
-    this.env = process.env.NODE_ENV === 'dev' ? 'dev' : 'prod'
-    console.log('this.env---', this.env);
-    
+    this.env = (process.env.NODE_ENV as keyof EnvConf) || 'dev';
+
     this.initConf()
   }
   initConf() {
     this.envConf = {
       dev: {
+        host: 'localhost',
+        user: 'root',
+        password: '157351',
+        database: 'dangdang',
+        port: 3306,
+      },
+      test: {
         host: 'localhost',
         user: 'root',
         password: '157351',
@@ -44,9 +52,9 @@ class Conf {
       },
     }
   }
-  getConf(): DbConConf
+  getConf(): DbConConf // 多个函数签名
   getConf(key: string): string
-  getConf(key: any = ''): any {
+  getConf(key: any = ''): any { // 只有一个函数实现
     if (this.isDbConConfKeys(key) && key.length > 0) {
       return this.envConf[this.env][key]
     } else {
@@ -55,13 +63,7 @@ class Conf {
     }
   }
   isDbConConfKeys(key: any): key is keyof DbConConf {
-    return (
-      key === 'host' ||
-      key === 'user' ||
-      key === 'password' ||
-      key === 'database' ||
-      key === 'port'
-    )
+    return key === 'host' || key === 'user' || key === 'password' || key === 'database' || key === 'port'
   }
 }
 
