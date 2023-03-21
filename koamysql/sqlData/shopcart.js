c44a9b1c498db5f7b61b671f10f0d0c5b10f283a
dragon <1573511441@qq.com>
2023-03-21 21:54:36 +0800

    feat: 购物车准备

--- koamysql/sqlData/shopcart.js
+++ koamysql/sqlData/shopcart.js
@@ -1,11 +1,11 @@
 // DROP TABLE IF EXISTS shopcart;
 // CREATE TABLE shopcart(
 //   shopcartid int NOT NULL AUTO_INCREMENT,
-//   bookisbn varchar(20) NOT NULL,
-//   bookname varchar(50) NOT NULL,
-//   bookpicname varchar(60) NOT NULL,
+//   bookisbn varchar(20) NOT NULL, // 图书表的主键
+//   bookname varchar(50) NOT NULL, // 图书名字
+//   bookpicname varchar(60) NOT NULL, // 图书的图片(一般需要)
 //   bookprice int NOT NULL,
-//   userid int NOT NULL,
+//   userid int NOT NULL, // 用户id的组件
 //   purcharsenum int NULL DEFAULT 0,
 //   PRIMARY KEY(shopcartid)
 // );
